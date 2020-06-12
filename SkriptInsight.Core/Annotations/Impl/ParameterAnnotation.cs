6d94f03bb4c36a07e8198134e2f13c03a59aff5a
NickAc <32451103+NickAcPT@users.noreply.github.com>
2020-06-12 22:50:32 +0100

    Complete @param annotation

--- SkriptInsight.Core/Annotations/Impl/ParameterAnnotation.cs
+++ SkriptInsight.Core/Annotations/Impl/ParameterAnnotation.cs
@@ -1,6 +1,22 @@
+using SkriptInsight.Core.Annotations.Parameters;
+using SkriptInsight.Core.Parser.Expressions;
+using SkriptInsight.Core.Parser.Types.Impl.Internal;
+
 namespace SkriptInsight.Core.Annotations.Impl
 {
-    public class ParameterAnnotation
+    [Alias("param")]
+    public class ParameterAnnotation : Annotation
     {
+        public ParameterAnnotation(string parameter, string description)
+        {
+            Parameter = parameter;
+            Description = description;
+        }
+
+        [Parameter]
+        public string Parameter { get; set; }
+
+        [Parameter]
+        public string Description { get; set; }
     }
 }
