ce0841c9c6e9fc3f5127f7c6a949809800031f54
NickAc <32451103+NickAcPT@users.noreply.github.com>
2020-06-13 01:51:41 +0100

    Introduce Condition Literals

--- SkriptInsight.Core/SyntaxInfo/SkriptCondition.cs
+++ SkriptInsight.Core/SyntaxInfo/SkriptCondition.cs
@@ -5,6 +5,26 @@ namespace SkriptInsight.Core.SyntaxInfo
     [DebuggerDisplay("{" + nameof(ClassName) + "}")]
     public class SkriptCondition : AbstractSyntaxElement
     {
+        public static readonly SkriptCondition TrueLiteral = new SkriptCondition
+        {
+            ClassName = "Boolean",
+            Patterns = new []
+            {
+                "(true|yes|on)"
+            },
+            AddonName = "Skript"
+        };
+        
+        public static readonly SkriptCondition FalseLiteral = new SkriptCondition
+        {
+            ClassName = "Boolean",
+            Patterns = new []
+            {
+                "(false|no|off)"
+            },
+            AddonName = "Skript"
+        };
+        
         public string ClassName { get; set; }
     }
 }
