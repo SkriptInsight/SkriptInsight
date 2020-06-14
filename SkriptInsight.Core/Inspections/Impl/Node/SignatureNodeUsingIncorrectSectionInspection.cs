c4398edecee79d7e3cee6d0faf6331e9ba9429ee
NickAc <32451103+NickAcPT@users.noreply.github.com>
2020-06-14 13:04:30 +0100

    Move SignatureFileNode check to CanInspect

--- SkriptInsight.Core/Inspections/Impl/Node/SignatureNodeUsingIncorrectSectionInspection.cs
+++ SkriptInsight.Core/Inspections/Impl/Node/SignatureNodeUsingIncorrectSectionInspection.cs
@@ -3,7 +3,6 @@ using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using SkriptInsight.Core.Extensions;
 using SkriptInsight.Core.Files;
 using SkriptInsight.Core.Files.Nodes;
-using SkriptInsight.Core.Parser.Expressions;
 
 namespace SkriptInsight.Core.Inspections.Impl.Node
 {
@@ -12,22 +11,19 @@ namespace SkriptInsight.Core.Inspections.Impl.Node
     {
         protected override bool CanInspect(SkriptFile file, int line, AbstractFileNode node)
         {
-            return node.MatchedSyntax == null;
+            return node.MatchedSyntax == null && node.GetType().IsSubclassOfRawGeneric(typeof(SignatureFileNode<>));
         }
 
         protected override void Inspect(SkriptFile file, int line, AbstractFileNode node)
         {
-            if (node.GetType().IsSubclassOfRawGeneric(typeof(SignatureFileNode<>)))
-            {
-                var signatureRequiresSection = node.GetType().GetCustomAttribute<SectionNodeAttribute>() != null;
-                var isSectionMismatch = node.IsSectionNode !=
-                                        signatureRequiresSection;
+            var signatureRequiresSection = node.GetType().GetCustomAttribute<SectionNodeAttribute>() != null;
+            var isSectionMismatch = node.IsSectionNode !=
+                                    signatureRequiresSection;
 
-                if (isSectionMismatch)
-                {
-                    AddProblem(DiagnosticSeverity.Error,
-                        $"{(signatureRequiresSection ? "E" : "Une")}xpected section at the end of this line", node);
-                }
+            if (isSectionMismatch)
+            {
+                AddProblem(DiagnosticSeverity.Error,
+                    $"{(signatureRequiresSection ? "E" : "Une")}xpected section at the end of this line", node);
             }
         }
     }
