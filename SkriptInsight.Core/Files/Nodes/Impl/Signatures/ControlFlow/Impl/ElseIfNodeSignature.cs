56e063ae5a054ce6b90c89e243148122e157a98c
NickAc <32451103+NickAcPT@users.noreply.github.com>
2020-07-16 19:41:13 +0100

    Commit missing code

---
+++ SkriptInsight.Core/Files/Nodes/Impl/Signatures/ControlFlow/Impl/ElseIfNodeSignature.cs
@@ -0,0 +1,9 @@
+namespace SkriptInsight.Core.Files.Nodes.Impl.Signatures.ControlFlow.Impl
+{
+    [ConditionalBasePrefix("else if")]
+    [SectionNode]
+    public class ElseIfNodeSignature : ConditionalBaseSignature<ElseIfNodeSignature>
+    {
+        
+    }
+}
